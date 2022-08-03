1dc068439987d3ad48aba61b5bb2ecaabc3117a9
imranahmed1990 <imranmarin1990@gmail.com>
2022-08-03 23:51:37 +0600

    added another task

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Service from './Service';
 
 const Services = () => {
@@ -7,18 +8,28 @@ const Services = () => {
     useEffect(() => {
         fetch('services.json')
             .then(res => res.json())
-            .then(data => setServices(data))
+            .then(data => setServices(data.slice(0, 3)))
     }, [])
     return (
         <div>
             <h2 className='text-3xl text-center font-bold my-20'>Our Services</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4 ml-10 mb-20'>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4 ml-14 '>
                 {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
                     ></Service>)
                 }
+            </div>
+
+            <div class="hero">
+                <div class="text-center hero-content">
+                    <div class="max-w-md">
+
+                        <Link to="/ourService"><button class="btn btn-outline">View more services</button></Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
